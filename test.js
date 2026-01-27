34422fd01a21ae083a57534fe03d1e67dff63988
Nick Wu <wu2264@purdue.edu>
2026-01-26 21:33:52 -0500

    testing embed

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("✅ External JS loaded from GitHub Pages");
+
+document.body.insertAdjacentHTML(
+  "beforeend",
+  "<div style='position:fixed;bottom:10px;right:10px;padding:8px 12px;background:#22c55e;color:white;font-weight:bold;z-index:999999;'>External JS Loaded</div>"
+);
